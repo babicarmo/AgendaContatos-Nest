55ac052b7c0ed033961e4fd47968750c1108a1d8
Bárbara Ferreira <babicarmof7@gmail.com>
2026-04-28 20:46:46 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ContatosModule } from './contatos/contatos.module';
+
+@Module({
+  imports: [ContatosModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
